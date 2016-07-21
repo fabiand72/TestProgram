ee60ae1dc2e8a56d7678572e4a824cce873ea25b
fluffyd72 <fabian.diaz@ttu.edu>
2016-07-21 10:56:25 +0100

    Add files via upload

---
+++ GitTestPro.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float radius;
+	cout << "Please enter a number for the radius" << endl;
+	cin >> radius;
+	cout << "The radius you entered is :" << radius << endl;
+
+	return 0;
+}
